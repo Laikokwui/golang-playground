07265a887f48021cf158c8249e3a552892135f7d
Laikokwei <kokwei325@gmail.com>
2022-09-27 16:17:26 +0800

    missing "

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
